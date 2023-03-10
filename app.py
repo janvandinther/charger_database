12f6ea40aafc2cac394ca09d9d8c29197d8cea4c
janvandinther <jan@vandinther.nl>
2023-03-10 08:55:53 +0000

    filepath

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import csv
 import pandas as pd
 import shutil
 
+entity_folder_path = Path(__file__).parent
+file_path = entity_folder_path.parent / 'assetdatabase.csv'
+
 class Parametrization(ViktorParametrization):
     asset_name          = TextField("Assetnaam")
     asset_brand         = TextField("Merk")
@@ -37,7 +40,7 @@ class Controller(ViktorController):
     def upload_entry(self, params, **kwargs):
         data = [params.asset_name, params.asset_brand, params.serial_number, params.buy_year, params.on_year, params.component_group, params.common_group, params.asset_description, location_name, params.location_coord]
         
-        with open('assetdatabase.csv', 'a', newline='') as file:
+        with open(file_path, 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(data)
         file.close()
@@ -47,7 +50,7 @@ class Controller(ViktorController):
     @PlotlyView("Assets", duration_guess=15)
     def get_plotly_view(self, params, **kwargs):
         
-        df = pd.read_csv('assetdatabase.csv')
+        df = pd.read_csv(file_path)
         
         fig = go.Figure(
             data=[go.Table(
