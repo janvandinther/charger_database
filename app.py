d8443d5d025a80ddea30e94f5be79bbe03f22dbc
janvandinther <jan@vandinther.nl>
2023-03-13 05:10:41 +0000

    veelnieuws

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from viktor import ViktorController, File
 from pathlib import Path
 from PIL import Image
-from viktor.parametrization import ViktorParametrization, TextField, IntegerField, TextAreaField, FileField, ActionButton, GeoPointField, OptionField, DateField
+from viktor.parametrization import ViktorParametrization, TextField, IntegerField, TextAreaField, FileField, ActionButton, GeoPointField, OptionField, DateField, Page, Section, LineBreak, DownloadButton
 from viktor.views import PlotlyView, PlotlyResult, MapView, MapResult, MapPoint
+from viktor.result import DownloadResult
 import plotly.graph_objects as go
 import csv
 import pandas as pd
@@ -12,21 +13,35 @@ entity_folder_path = Path(__file__)
 file_path = entity_folder_path.parent / 'assetdatabase.csv'
 
 class Parametrization(ViktorParametrization):
-    asset_name          = TextField("Assetnaam")
-    asset_brand         = TextField("Merk")
-    serial_number       = IntegerField("Serienummer")
-    buy_year            = DateField("Bouwjaar")
-    on_year             = DateField("Inbedrijfname")
-    component_group     = OptionField('Component group', options=['Generator', 'Motor', 'Common', 'Other'], default='Other')    
-    common_group        = OptionField('Common group', options=['None', 'AC/DC', 'Air', 'Fire Fighting', 'Fuel', 'Lube Oil System', 'Monitoring and Control', 'Steam', 'Switchgear' , 'Waste Oil', 'Water Treatment and Cooling', 'Other'], default='None')
-    location_name       = TextField("Locatienaam")
-    location_coord      = GeoPointField('Locatie')
-    asset_description   = TextAreaField("Beschrijving")
-    asset_photo_1       = FileField("Foto 1")
-    asset_photo_2       = FileField("Foto 2")
+    page_input =    Page("Voeg toe & Pas aan", views = ['asset_view','location_view'])
+    page_results =  Page("Overzicht Assets", views = ['assets_map_view'])
     
+    page_input.section_input = Section("Voeg asset toe")
+    page_input.section_change = Section("Pas asset aan")
     
-    upload_button       = ActionButton("Upload naar database", method='upload_entry')
+    page_input.section_input.asset_name          = TextField("Assetnaam")
+    page_input.section_input.asset_brand         = TextField("Merk")
+    page_input.section_input.serial_number       = IntegerField("Serienummer")
+    page_input.section_input.buy_year            = IntegerField("Bouwjaar")
+    page_input.section_input.on_year             = IntegerField("Inbedrijfname")
+    page_input.section_input.component_group     = OptionField('Component groep', options=['Generator', 'Motor', 'Common', 'Other'], default='Other')    
+    page_input.section_input.common_group        = OptionField('Common groep', options=['None', 'AC/DC', 'Air', 'Fire Fighting', 'Fuel', 'Lube Oil System', 'Monitoring and Control', 'Steam', 'Switchgear' , 'Waste Oil', 'Water Treatment and Cooling', 'Other'], default='None')
+    page_input.section_input.location_name       = TextField("Locatienaam")
+    page_input.section_input.location_coord      = GeoPointField('Locatie')
+    page_input.section_input.asset_description   = TextAreaField("Beschrijving")
+    page_input.section_input.asset_photo_1       = FileField("Foto 1")
+    page_input.section_input.asset_photo_2       = FileField("Foto 2")
+    page_input.section_input.upload_button       = ActionButton("Upload naar database", method='upload_entry')
+    
+    page_input.section_change.select_serialnumber   = IntegerField('Serienummer')
+    page_input.section_change.lb1                   = LineBreak()
+    page_input.section_change.select_attribute      = OptionField('Eigenschap', options = ['Assetnaam', 'Merk', 'Serienummer','Bouwjaar','Inbedrijfname','Component groep','Common groep', 'Locatienaam','Locatie','Beschrijving'])
+    page_input.section_change.new_value             = TextField("Nieuwe waarde")
+    page_input.section_change.new_location           = GeoPointField('Nieuwe locatie')
+    page_input.section_change.change_button         = ActionButton("Pas aan", method='change_entry')
+    page_input.section_change.delete_button         = ActionButton("Verwijder uit database", method='delete_entry')
+    
+    page_results.download_button       = DownloadButton("Download", method='download_file')
     
     pass
 
@@ -34,21 +49,63 @@ class Parametrization(ViktorParametrization):
 class Controller(ViktorController):
     viktor_enforce_field_constraints = True  # Resolves upgrade instruction https://docs.viktor.ai/sdk/upgrades#U83
 
-    label = 'My Entity Type'
+    label = 'ESB Asset Inventarisator'
     parametrization = Parametrization
     
     def upload_entry(self, params, **kwargs):
-        data = [params.asset_name, params.asset_brand, params.serial_number, params.buy_year, params.on_year, params.component_group, params.common_group, params.asset_description, params.location_name, params.location_coord]
+        data = [
+            params.page_input.section_input.asset_name, 
+            params.page_input.section_input.asset_brand, 
+            params.page_input.section_input.serial_number, 
+            params.page_input.section_input.buy_year, 
+            params.page_input.section_input.on_year, 
+            params.page_input.section_input.component_group, 
+            params.page_input.section_input.common_group, 
+            params.page_input.section_input.asset_description, 
+            params.page_input.section_input.location_name, 
+            params.page_input.section_input.location_coord
+            ]
         
-        with open(file_path, 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(data)
-        file.close()
+        df = pd.read_csv(file_path)
+        df.loc[len(df)] = data
+        df.to_csv(file_path, index=False)
+        print(df.columns)
+        
+    
+    def change_entry(self, params, **kwargs):
+        if params.page_input.section_change.select_attribute in ["Assetnaam", "Merk", "Component groep", "Common groep","Beschrijving"]:
+            df = pd.read_csv(file_path)
+            print(df.columns)
+            index = df.loc[df['Serienummer'] == params.page_input.section_change.select_serialnumber].index[0]
+            df.at[index, params.page_input.section_change.select_attribute] = params.page_input.section_change.new_value
+            df.to_csv(file_path, index=False)
+        else:
+            if params.page_input.section_change.select_attribute != "Locatie":
+                df = pd.read_csv(file_path)
+                index = df.loc[df['Serienummer'] == params.page_input.section_change.select_serialnumber].index[0]
+                df.at[index, params.page_input.section_change.select_attribute] = int(params.page_input.section_change.new_value)
+                df.to_csv(file_path, index=False)
+            else:
+                df = pd.read_csv(file_path)
+                index = df.loc[df['Serienummer'] == params.page_input.section_change.select_serialnumber].index[0]
+                df.at[index, params.page_input.section_change.select_attribute] = params.page_input.section_change.new_location
+                df.to_csv(file_path, index=False)
+                
+    def delete_entry(self,params, **kwargs):
+        df = pd.read_csv(file_path)
+        index = df.loc[df['Serienummer'] == params.page_input.section_change.select_serialnumber].index[0]
+        df.drop(index, inplace=True)
+        df.to_csv(file_path, index=False)
+        
+    def download_file(self, params, **kwargs):
+       
+        file = File.from_path(file_path)
+        return DownloadResult(file, 'asset_database_EBS.csv')
             
     
 
     @PlotlyView("Assets", duration_guess=15)
-    def get_plotly_view(self, params, **kwargs):
+    def asset_view(self, params, **kwargs):
         
         df = pd.read_csv(file_path)
         
@@ -65,7 +122,14 @@ class Controller(ViktorController):
         return PlotlyResult(fig.to_json())
     
     @MapView('Locatie Selectie', duration_guess=1)
-    def get_map_view(self, params, **kwargs):
-        marker = MapPoint(5.821153034001172, -55.16400368408251)
+    def location_view(self, params, **kwargs):
+        marker = MapPoint(5.905264411741596, -57.02917512726031)
         features = [marker]
         return MapResult(features)
+    
+    @MapView('Assets op kaart', duration_guess=1)
+    def assets_map_view(self, params, **kwargs):
+        marker = MapPoint(5.905264411741596, -57.02917512726031)
+        features = [marker]
+        return MapResult(features)
+
